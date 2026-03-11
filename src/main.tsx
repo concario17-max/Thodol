8c232f29ba0cfcd9c8dba40811313be26e749c35
concario17-max <concario17@gmail.com>
2026-03-11 12:21:02 +0900

    fix(ui): wrap App in ThemeProvider and UIProvider to prevent context errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ThemeProvider } from './context/ThemeContext.tsx';
+import { UIProvider } from './context/UIContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <ThemeProvider>
+            <UIProvider>
+                <App />
+            </UIProvider>
+        </ThemeProvider>
     </StrictMode>,
 );
